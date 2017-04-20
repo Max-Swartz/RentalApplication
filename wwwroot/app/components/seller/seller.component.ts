df4a8420c18f289dc4deb60fed946fcd52bfd73c
Megan Holstein <meganeholstein@icloud.com>
2017-04-20 00:19:11 -0400

    Added property html and CSS

---
+++ wwwroot/app/components/seller/seller.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; 
+
+@Component({     
+    selector: 'contract',     
+    templateUrl: './app/components/contract/contract.component.html' 
+}) 
+
+export class ContractComponent { 
+    
+}
