e80e6952c6cb682e1f36d69faf1f5723b12df721
Amit Rubinstein <AmitRubinstein@Amits-MacBook-Pro.local>
2017-04-19 21:06:06 -0400

    lab 7

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavBarComponent } from './components/navbar/navbar.component'
 import { ContractComponent } from './components/contract/contract.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from "./auth.service";
+import { AuthHttp } from "./auth.http";
 
 @NgModule({
     declarations: [
@@ -29,7 +30,7 @@ import { AuthService } from "./auth.service";
         AppRouting
     ],
     providers: [  
-        AuthService
+        AuthService,
         AuthHttp
     ],
     bootstrap: [
